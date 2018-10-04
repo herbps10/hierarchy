dd3c29387d6bd9e234885d8b8a11b796283feada
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2018-10-04 16:41:43 -0400

    Added some doc.

--- R/hierarchy.R
+++ R/hierarchy.R
@@ -6,7 +6,8 @@
 #' 
 #' @field .levels names of the (child) levels in the hierarchy
 #' @field .table table representing the hierarchy
-#' @export
+#' @export hierarchy_factory
+#' @exportClass hierarchy
 hierarchy_factory = methods::setRefClass(Class = "hierarchy",
   fields = list(
     .levels = "character",
@@ -15,7 +16,6 @@ hierarchy_factory = methods::setRefClass(Class = "hierarchy",
   methods = list(
     initialize = function(files, ...) {
       "Construct hierarchy from given relationship files."
-      methods::callSuper(...)
       sh = spatial_hierarchy(files)
       .self$.table = sh
       .self$.levels = region_names(sh)
@@ -24,10 +24,10 @@ hierarchy_factory = methods::setRefClass(Class = "hierarchy",
       "Method to retrieve the state of the hierarchy at a given point in time (date)"
       table = .self$.table
       if (!is.null(timepoint)) {
-	filter = 
-	  (.self$.table[['start_date']] <= timepoint | is.na(.self$.table[['start_date']])) &
+        filter = 
+          (.self$.table[['start_date']] <= timepoint | is.na(.self$.table[['start_date']])) &
           (.self$.table[['end_date']]    > timepoint | is.na(.self$.table[['end_date']]))
-	table = table[filter,]
+        table = table[filter,]
       }
       return(table)
     },
@@ -38,6 +38,7 @@ hierarchy_factory = methods::setRefClass(Class = "hierarchy",
       return(size)
     },
     get_level_idxs = function(level_name, timepoint = NULL) {
+      "Get the indexes for all parameters at a given level."
       table = get_table(timepoint)
       region_level = table[['region_level']]
       idxs = which(region_level == level_name)     
@@ -45,12 +46,21 @@ hierarchy_factory = methods::setRefClass(Class = "hierarchy",
 
     },
     get_parent_idxs = function(idxs, timepoint = NULL) {
+      "Get the indexes of the parents of a given set of indexes.  This is 
+       relevant if the hierarchy is not cleanly split into K levels for all
+       indexes."
       table = get_table(timepoint)
       parent_id = table[['parent_id']][idxs]
       idxs = sapply(parent_id, get_id_idx, table = table, USE.NAMES=FALSE)
       return(idxs)
     },
     get_hierarchy_idxs = function(timepoint = NULL) {
+      "Get a matrix with the indexes of all levels of the hierarchy, 
+       with onoe entry per bottom-level node.  This format produces
+       redundancy at the higher levels (a given higher-level parameter
+       will appear multiple times) because that allows it to represent
+       the hierarchy clearly even when not all parts have the same number
+       of levels."
       table = get_table(timepoint)
       idxs = get_leaf_idxs(table)
       ml_idxs = idxs
@@ -65,6 +75,10 @@ hierarchy_factory = methods::setRefClass(Class = "hierarchy",
       return(ml_idxs[,1:nc])
     },
     get_vectorized_hierarchy = function(timepoint = NULL) {
+      "Get a list of three vectors: 1) the matrix of all hierarchy
+       indexes collapsed into a single vector; 2) the location in 
+       the vector where each level starts; and 3) the location (index)
+       in the vector where each level ends."
       idxs = get_hierarchy_idxs(timepoint)
       idxs_l = apply(idxs, 1, list)
       idxs_l = lapply(idxs_l, unlist)
@@ -75,8 +89,8 @@ hierarchy_factory = methods::setRefClass(Class = "hierarchy",
       stops = (starts + lengths - 1)
       return(list(
         idx = unlist(idxs_l),
-	start = starts,
-	end = stops
+        start = starts,
+        end = stops
       ))
     }
   )
